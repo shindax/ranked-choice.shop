c5a3db2aef2f9702734a8ef2d698bc7ddad343ff
Shindax <shindax@mail.ru>
2022-01-27 13:34:53 +0700

    Product Entity created

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,28 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+        $em = $this -> getDoctrine() -> getManager();
+        $productList = $em -> getRepository( Product::class ) -> findAll();
+        dd( $productList );
+
         return $this->render('main/default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    #[Route('/product-add', name: 'product_add')]
+    public function productAdd(): Response
+    {
+        $product = new Product();
+        $product -> setTitle('Product'.rand(1,100));
+        $product -> setDescription('smth');
+        $product -> setPrice( 10 );
+        $product -> setQuantity( 1 );
+
+        $em = $this -> getDoctrine() -> getManager();
+        $em -> persist( $product );
+        $em -> flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
